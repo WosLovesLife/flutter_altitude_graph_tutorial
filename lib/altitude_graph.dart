f53dffa7069f30fd71f3c325ca79049071c500d7
WosLovesLife <zhang00h00@126.com>
2018-07-25 09:43:29 +0800

    5. 创建控件所需的数据模型

--- lib/altitude_graph.dart
+++ lib/altitude_graph.dart
@@ -1,5 +1,45 @@
 import 'package:flutter/material.dart';
 
+const Color kLabelTextColor = Colors.white;
+
+class AltitudePoint {
+  /// 当前点的名字, 例如: xx镇
+  String name;
+
+  /// 当前点的级别, 用于根据缩放级别展示不同的地标标签.
+  int level;
+
+  /// `point.x`表示当前点距离上一个点的距离. `point.y`表示当前点的海拔
+  Offset point;
+
+  /// 地标标签的背景色
+  Color color;
+
+  /// 用于绘制文字, 存在这里是为了避免每次绘制重复创建.
+  TextPainter textPainter;
+
+  AltitudePoint(this.name, this.level, this.point, this.color, {this.textPainter}) {
+    if (name == null || name.isEmpty || textPainter != null) return;
+
+    // 向String插入换行符使文字竖向绘制
+    var splitMapJoin = name.splitMapJoin('', onNonMatch: (m) {
+      return m.isNotEmpty ? "$m\n" : "";
+    });
+    splitMapJoin = splitMapJoin.substring(0, splitMapJoin.length - 1);
+
+    this.textPainter = TextPainter(
+      textDirection: TextDirection.ltr,
+      text: TextSpan(
+        text: splitMapJoin,
+        style: TextStyle(
+          color: kLabelTextColor,
+          fontSize: 8.0,
+        ),
+      ),
+    )..layout();
+  }
+}
+
 class AltitudeGraphView extends StatefulWidget {
   @override
   _AltitudeGraphViewState createState() => new _AltitudeGraphViewState();
@@ -33,7 +73,7 @@ class _AltitudeGraphViewState extends State<AltitudeGraphView> {
   }
 }
 
-class AltitudePainter extends CustomPainter{
+class AltitudePainter extends CustomPainter {
   Paint linePaint = Paint()..color = Colors.red;
 
   @override
